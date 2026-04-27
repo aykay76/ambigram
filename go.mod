863d08b9ee6d84d3ae53caefadc778d05a84d16b
Alan Kelly <alan.kelly.london@gmail.com>
2026-04-27 20:51:43 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/aykay76/ambigram
+
+go 1.26.1
+
+require (
+	github.com/soypat/geometry v0.0.0-20251107203642-291c5648d529
+	github.com/soypat/gsdf v0.0.0-20260113194216-a2a2988c3a75
+)
+
+require (
+	github.com/chewxy/math32 v1.11.1 // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw v0.0.0-20250301202403-da16c1255728 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20250301202403-da16c1255728 // indirect
+	github.com/soypat/glgl v0.0.0-20250930033525-aafa471e3c50 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
+	golang.org/x/image v0.22.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"runtime"
+
+	"github.com/soypat/geometry/ms2"
+	"github.com/soypat/geometry/ms3"
+	"github.com/soypat/gsdf"
+	"github.com/soypat/gsdf/forge/textsdf"
+	"github.com/soypat/gsdf/glbuild"
+	"github.com/soypat/gsdf/gsdfaux"
+)
+
+const (
+	stl             = "gasket.stl"
+	visualization   = "gasket.glsl"
+	visualization2D = "gasket2D.png"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+// scene generates the 3D object for rendering.
+func scene(bld *gsdf.Builder) (glbuild.Shader3D, error) {
+	// We create the cover of Gödel, Escher, Bach: an Eternal Golden Braid.
+	var f textsdf.Font
+	f.Configure(textsdf.FontConfig{
+		RelativeGlyphTolerance: 0.01,
+		Builder:                bld,
+	})
+	err := f.LoadTTFBytes(textsdf.ISO3098TTF())
+	if err != nil {
+		return nil, err
+	}
+	G, _ := f.Glyph('G')
+	E, _ := f.Glyph('E')
+	B, _ := f.Glyph('B')
+
+	bbG := G.Bounds()
+	bbE := E.Bounds()
+	bbB := B.Bounds()
+	fmt.Println(bbG, bbE, bbB)
+	// return nil, errors.New("basdasd")
+	szG := bbG.Size()
+	szE := bbE.Size()
+	szB := bbB.Size()
+
+	// Match center between letters.
+	G = bld.Translate2D(G, -szG.X/2, -szG.Y/2)
+	E = bld.Translate2D(E, -szE.X/2, -szE.Y/2)
+	B = bld.Translate2D(B, -szB.X/2, -szB.Y/2)
+	const round1 = 0.01
+	G = bld.Offset2D(G, -round1)
+	E = bld.Offset2D(E, -round1)
+	B = bld.Offset2D(B, -round1)
+
+	// GEB size. Scale all letters to match size.
+	szz := ms2.MaxElem(szG, ms2.MaxElem(szE, szB)).Max()
+	sz := ms2.Vec{X: szz, Y: szz}
+	sclG := ms2.DivElem(sz, szG)
+	sclE := ms2.DivElem(sz, szE)
+	sclB := ms2.DivElem(sz, szB)
+
+	// Create 3D letters.
+	L := sz.Max()
+	G3 := bld.Extrude(G, L)
+	E3 := bld.Extrude(E, L)
+	B3 := bld.Extrude(B, L)
+
+	// Non-uniform scaling to fill letter intersections.
+	G3 = bld.Transform(G3, ms3.ScalingMat4(ms3.Vec{X: sclG.X, Y: sclG.Y, Z: 1}))
+	E3 = bld.Transform(E3, ms3.ScalingMat4(ms3.Vec{X: sclE.X, Y: sclE.Y, Z: 1}))
+	B3 = bld.Transform(B3, ms3.ScalingMat4(ms3.Vec{X: sclB.X, Y: sclB.Y, Z: 1}))
+
+	const round2 = 0.025
+	G3 = bld.Offset(G3, -round2)
+	E3 = bld.Offset(E3, -round2)
+	B3 = bld.Offset(B3, -round2)
+
+	// Orient letters.
+	const deg90 = math.Pi / 2
+	GEB1 := bld.Intersection(G3, bld.Rotate(E3, deg90, ms3.Vec{Y: 1}))
+	GEB1 = bld.Intersection(GEB1, bld.Rotate(B3, -deg90, ms3.Vec{X: 1}))
+
+	GEB2 := bld.Intersection(E3, bld.Rotate(G3, deg90, ms3.Vec{Y: 1}))
+	GEB2 = bld.Intersection(GEB2, bld.Rotate(B3, -deg90, ms3.Vec{X: 1}))
+
+	GEB2 = bld.Translate(GEB2, 0, GEB2.Bounds().Size().Y*1.5, 0)
+
+	return bld.Union(GEB1, GEB2), bld.Err()
+}
+
+func main() {
+	var (
+		useGPU     bool
+		resolution float64
+		flagResDiv uint
+	)
+	flag.BoolVar(&useGPU, "gpu", false, "enable GPU usage")
+	flag.Float64Var(&resolution, "res", 0, "Set resolution in shape units. Useful for setting the minimum level of detail to a fixed amount for final result. If not set resdiv used [mm/in]")
+	flag.UintVar(&flagResDiv, "resdiv", 200, "Set resolution in bounding box diagonal divisions. Useful for prototyping when constant speed of rendering is desired.")
+	flag.Parse()
+
+	var bld gsdf.Builder
+	bld.SetFlags(gsdf.FlagNoShaderBuffers)
+	object, err := scene(&bld)
+	// object = bld.Scale(object, 0.3)
+	// if err != nil {
+	// 	log.Fatal("creating scene:", err)
+	// }
+	// fmt.Println("Running UI... compiling text shaders may take a while...")
+
+	fpstl, err := os.Create(stl)
+	if err != nil {
+		fmt.Println("Error creating STL file:", err)
+	}
+	defer fpstl.Close()
+	fpvis, err := os.Create(visualization)
+	if err != nil {
+		fmt.Println("Error creating visualization file:", err)
+	}
+	defer fpvis.Close()
+
+	err = gsdfaux.RenderShader3D(object, gsdfaux.RenderConfig{
+		STLOutput:    fpstl,
+		VisualOutput: fpvis,
+		Resolution:   float32(float64(object.Bounds().Diagonal()) / float64(200)),
+		UseGPU:       useGPU,
+	})
+	// err = gsdfaux.UI(shape, gsdfaux.UIConfig{
+	// 	Width:  800,
+	// 	Height: 600,
+	// })
+	// if err != nil {
+	// 	log.Fatal("UI:", err)
+	// }
+}
